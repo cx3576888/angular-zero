d5baf8f7f82f23f6b455cd173dba1d65a4ece74b
yen <cx3576888@gmail.com>
2020-03-05 01:20:29 +0800

    HW1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,19 @@ export class AppComponent {
   title = 'app';
   url = 'http://blog.miniasp.com/';
   imgurl = '/assets/images/logo.png';
+  characters = 0;
   constructor() {
   }
   changeTitle(altKey: boolean) {
     if (altKey) {
       this.title  = 'The Will Will Web';
+    }
+  }
+  onKey($event: KeyboardEvent) {
+    this.characters = $event.target.value.length;
+    if ($event.keyCode === 27) {
+      $event.target.value = '';
+      this.characters = 0;
     }
   }
 }
